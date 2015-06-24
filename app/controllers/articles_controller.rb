72266db607374ad49033590a4ad850e970d9f0b8
RaySuhyunLee <raysuhyunlee@gmail.com>
2015-06-25 01:53:37 +0900

    show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,16 @@
 class ArticlesController < ApplicationController
+
+	def show
+		filename = params[:title]
+		if File.exists?('/articles/'+filename)
+			@contents = File.read('/articles/'+filename)
+		else
+			@contents = nil
+		end
+	end
+
+	def save
+		filename = params[:title]
+		contents = params[:contents]
+	end
 end
